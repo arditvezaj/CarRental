fd599774e2214ecc0dcfc5ee1c9baa0979a47f56
arditvezaj <arditvezaj1@gmail.com>
2023-07-25 09:40:20 +0200

    error managing

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
 import Home from "./src/screens/Home";
 import Login from "./src/screens/Login";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      <Login />
-      {/* <Home /> */}
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
