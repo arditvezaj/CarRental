f76abd091b4314a2c6ab0a4b18b636eca1b7ec84
arditvezaj <arditvezaj1@gmail.com>
2023-08-28 23:00:40 +0200

    styles improvement in cardetails

--- src/components/CarInfos.js
+++ src/components/CarInfos.js
@@ -12,16 +12,15 @@ const CarInfos = ({
   imageUrl,
 }) => {
   return (
-    <View style={styles.container}>
-      <SafeAreaView>
+    <SafeAreaView>
+      <View style={styles.container}>
         <Image source={imageUrl} style={styles.image} />
-        <Text style={styles.carName}>{name}</Text>
-        <View style={styles.textOutline}>
-          <Text style={styles.price}>Price: </Text>
+        <View style={{ flexDirection: "row", justifyContent: "space-between", width: "87%" }}>
+          <Text style={styles.carName}>{name}</Text>
           <Text style={styles.price}>${price}/day </Text>
         </View>
         <View>
-          <Text style={styles.textKey}>Specifications</Text>
+          <Text style={styles.price}>Specifications</Text>
           <View style={styles.boxesContainer}>
             <View style={styles.box}>
               <Image
@@ -32,7 +31,7 @@ const CarInfos = ({
             </View>
             <View style={styles.box}>
               <Image
-                source={require("../../assets/images/icons/seatss.png")}
+                source={require("../../assets/images/icons/seats.png")}
                 style={styles.icon}
               />
               <Text style={styles.boxText}>{seats} seater</Text>
@@ -46,32 +45,35 @@ const CarInfos = ({
             </View>
           </View>
         </View>
-        {/* <View style={styles.textOutline}>
-          <Text style={styles.textKey}>Transmission: </Text>
-          <Text style={styles.text}>{transmission}</Text>
-        </View> */}
-        {/* <View style={styles.textOutline}>
-          <Text style={styles.textKey}>Seats: </Text>
-          <Text style={styles.text}>{seats}</Text>
-        </View> */}
-        {/* <View style={styles.textOutline}>
-          <Text style={styles.textKey}>Fuel type: </Text>
-          <Text style={styles.text}>{fuel}</Text>
-        </View> */}
-        <View style={styles.textOutline}>
-          <Text style={styles.textKey}>Engine power: </Text>
-          <Text style={styles.text}>{engine}</Text>
-        </View>
-        <View style={styles.textOutline}>
-          <Text style={styles.textKey}>Milleage: </Text>
-          <Text style={styles.text}>{km}</Text>
+        <View style={styles.date}>
+          <Image
+            source={require("../../assets/images/icons/engine.png")}
+            style={styles.smallIcon}
+          />
+          <Text>Engine power: </Text>
+          <Text>{engine} cc</Text>
         </View>
         <View style={styles.date}>
+          <Image
+            source={require("../../assets/images/icons/mileage.png")}
+            style={styles.smallIcon}
+          />
+          <Text>Mileage: </Text>
+          <Text>{km} km</Text>
+        </View>
+        <View style={styles.date}>
+          <Image
+            source={require("../../assets/images/icons/clock.gif")}
+            style={styles.smallIcon}
+          />
           <Text>Available date: </Text>
           <Text>{date}</Text>
         </View>
-      </SafeAreaView>
-    </View>
+      </View>
+      <View style={styles.bottomButton}>
+        <Text style={styles.buttonText}>Book Now</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -81,6 +83,7 @@ const styles = StyleSheet.create({
   boxesContainer: {
     flexDirection: "row",
     gap: 10,
+    marginVertical: 5,
   },
   image: {
     borderRadius: 6,
@@ -89,6 +92,11 @@ const styles = StyleSheet.create({
     width: 40,
     height: 40,
     marginBottom: 10,
+  },
+  smallIcon: {
+    width: 30,
+    height: 30,
+    marginRight: 15,
   },
   box: {
     width: 95,
@@ -112,8 +120,10 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "700",
     color: "white",
+    marginVertical: 5,
   },
   container: {
+    flex: 1,
     margin: 20,
     justifyContent: "center",
     alignItems: "center",
@@ -124,9 +134,27 @@ const styles = StyleSheet.create({
   textKey: { color: "white" },
   text: { color: "white", fontWeight: "bold" },
   date: {
-    width: "100%",
-    padding: 10,
+    width: "87%",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    paddingVertical: 8,
     backgroundColor: "white",
-    borderRadius: 6
-  }
+    borderRadius: 6,
+    marginVertical: 5,
+  },
+  bottomButton: {
+    flex: 1,
+    marginTop: 115,
+    backgroundColor: "grey",
+    alignItems: "center",
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15
+  },
+  buttonText: {
+    padding: 18,
+    fontWeight: "700",
+    fontSize: 30,
+    marginBottom: 20
+  },
 });
